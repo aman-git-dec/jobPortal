59c145ca4fccf67f389c30070ec030130308a64d
aman <157161327+aman-git-dec@users.noreply.github.com>
2026-05-23 20:00:59 +0530

    defined routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,54 @@
 //import React from 'react';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+//import { Button } from "./components/ui/Button";
+import AppLayout from "./layouts/app-layout";
+import LandingPage from "./pages/landing";
+import Onboarding from "./pages/onboarding";
+import Joblisting from "./pages/job-listing";
+import JobPage from "./pages/job";
+import PostJob from "./pages/post-job";
+import SavedJobs from "./pages/saved-job";
+import MyJobs from "./pages/myjobs";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout/>,
+    children:[
+      {
+        path:'/',
+        element:<LandingPage/>,
+      },
+      {
+        path:'/onboarding',
+        element:<Onboarding/>,
+      },
+      {
+        path:'/jobs',
+        element:<Joblisting/>,
+      },
+      {
+        path:'/job/:id',
+        element:<JobPage/>,
+      },
+      {
+        path:'/post-job',
+        element:<PostJob/>,
+      },
+      {
+        path:'/saved-jobs',
+        element:<SavedJobs/>,
+      },
+      {
+        path:'/my-jobs',
+        element:<MyJobs/>,
+      },
+    ],
+  },
+])
 
 function App() {
   return(
-    <div>
-      <h1>job portal</h1>
-    </div>
+    <RouterProvider router = {router}/>
   )
 }
 
