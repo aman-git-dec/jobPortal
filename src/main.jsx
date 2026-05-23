21434bee65a66e7168c34ac531a05b9c20bed12f
aman <157161327+aman-git-dec@users.noreply.github.com>
2026-05-24 00:00:32 +0530

    clerk authentication setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import { ClerkProvider } from "@clerk/clerk-react";
+import { shadesOfPurple } from "@clerk/themes";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} 
+    appearance={
+      {
+        baseTheme:shadesOfPurple
+      }
+    }>
+      <App />
+    </ClerkProvider>
+  </StrictMode>
+);
