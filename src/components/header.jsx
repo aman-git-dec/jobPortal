fef0d6b697909713a120e9389ad4a3689a565db5
aman <157161327+aman-git-dec@users.noreply.github.com>
2026-05-25 21:31:57 +0530

    onboarding page

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,18 +1,93 @@
-import {Link} from 'react-router-dom';
-import {Button} from "./ui/button";
-const Header = () => {
-    return (
-        <>
-            <nav className='py-4 flex justify-between item-center'>
-                <Link>
-                    <img src='logo.png' className ='h-20' />
-                </Link>
-                <Button variant="outline">Login</Button>
-            </nav>
+import { Link, useSearchParams } from "react-router-dom";
+import { Button } from "./ui/button";
 
-            
-        </>
-    )
+import {
+  SignIn,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/clerk-react";
+import { BriefcaseBusiness, Heart, PenBox } from "lucide-react";
+import { useState, useEffect } from "react";
+
+const Header = () => {
+  const [showSignIn, setshowSignIn] = useState(false);
+
+  const [search, setSearch] = useSearchParams();
+
+  useEffect(()=>{
+    if(search.get('sign-in')){
+      setshowSignIn(true);
+    }
+  },[search]);
+
+  const handleOverlayClick = (e) => {
+    if (e.target === e.currentTarget){
+      setshowSignIn(false);
+      setSearch({});
+    }
+  };
+
+  return (
+    <>
+      <nav className="py-4 flex justify-between items-center">
+        <Link to="/">
+          <img src="logo.png" className="h-20" />
+        </Link>
+
+
+
+        <div className="flex gap-4">
+          <SignedOut>
+            <Button variant="outline" onClick={() => setshowSignIn(true)}>
+              Login
+            </Button>
+
+          </SignedOut>
+
+
+
+          <SignedIn>
+            {/* add a condition here */}
+            <Button variant="destructive" className="rounded-full">
+              <PenBox size={20} className="mr-2" />
+              Post a job
+            </Button>
+            <Link to="/post-job"></Link>
+            <UserButton appearance={{
+              elements:{
+                avatarBox:"w-10 h-10",
+              },
+            }}>
+              <UserButton.MenuItems>
+                <UserButton.Link
+                  label="My Jobs"
+                  labelIcon={<BriefcaseBusiness size={15}/>}
+                  href="/my-jobs"
+                />
+                <UserButton.Link
+                  label="Saved Jobs"
+                  labelIcon={<Heart size={15}/>}
+                  href="/saved-jobs"
+                />
+              </UserButton.MenuItems>
+            </UserButton>
+          </SignedIn>
+        </div>
+      </nav>
+      {showSignIn && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+          onClick={handleOverlayClick}
+        >
+        <SignIn
+        signUpForceRedirectUrl="/onboarding"
+        fallbackRedirectUrl="/onboarding"
+        />
+      </div>)}
+    </>
+  );
 };
 
 export default Header;
