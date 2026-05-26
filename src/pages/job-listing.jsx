f573a272d1c35cb212684b2340d1fc4e66c47215
aman <157161327+aman-git-dec@users.noreply.github.com>
2026-05-26 19:48:37 +0530

    useFetch custom hook

--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -1,6 +1,25 @@
-const Joblisting = () =>{
+
+import { getJobs } from "@/api/apiJobs";
+
+import useFetch from "@/hooks/useFetch";
+import { useEffect } from "react";
+
+const JobListing = () =>{
+
+    const {
+        fn:fnJobs,
+        data:dataJobs,
+        loading:loadingJobs
+    } = useFetch(getJobs,{})
+
+    console.log(dataJobs)
+
+    useEffect(()=>{
+        fnJobs()
+    },[])
+    
     return(
         <div>JOb listing</div>
     )
 };
-export default Joblisting;
+export default JobListing;
